08c69e8e9ff66d8842327469136f860a6105838f
Liam Xue <liamxue2002@gmail.com>
2021-12-08 12:07:28 -0500

    Delete add_task.bat

--- add_task.bat
+++
@@ -1,4 +0,0 @@
-echo on
-echo "started"
-schtasks /create /tn UTCB /tr %0\..\Run.bat /sc monthly /st 11:08 /d 8 /sd 2021/12/08 /ed 2021/12/08 /et 11:10 /z /ri 0
-set /p exitkey= "Press any key to exit..."
